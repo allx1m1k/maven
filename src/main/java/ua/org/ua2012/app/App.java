604587f08428b3fb47fec39fe617dcee3d7f4960
allx1m1k <UkraineChampion2012>
2018-07-02 00:14:03 +0300

    initial commit

---
+++ src/main/java/ua/org/ua2012/app/App.java
@@ -0,0 +1,13 @@
+package ua.org.ua2012.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Hello Dima!" );
+    }
+}
